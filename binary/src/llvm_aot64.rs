a7c3ff05afb349db5ed7424a2da765d5c719647b
Xuejie Xiao <xxuejie@gmail.com>
2023-05-18 05:14:10 +0000

    Time LLVM AOT engine

--- binary/src/llvm_aot64.rs
+++ binary/src/llvm_aot64.rs
@@ -3,6 +3,7 @@ use ckb_vm_contrib::{
     llvm_aot::{DlSymbols, LlvmAotMachine, LlvmCompilingMachine},
 };
 use std::process::Command;
+use std::time::SystemTime;
 use tempfile::Builder;
 
 fn main() {
@@ -11,6 +12,7 @@ fn main() {
     let code = std::fs::read(&filename).unwrap().into();
     let args: Vec<Bytes> = args.into_iter().map(|a| a.into()).collect();
 
+    let t0 = SystemTime::now();
     LlvmCompilingMachine::initialize().expect("initialize");
     let compiling_machine =
         LlvmCompilingMachine::load(&filename, &code, "test_suite_aot", &(|_| 0), false)
@@ -19,6 +21,9 @@ fn main() {
     let object_path = object_file.path().to_str().expect("tempfile");
     let object = compiling_machine.aot(true).expect("aot");
     std::fs::write(object_path, &object).expect("write");
+    let t1 = SystemTime::now();
+    let duration = t1.duration_since(t0).expect("time went backwards");
+    println!("Time to generate object: {:?}", duration);
 
     let library_file = Builder::new().suffix(".so").tempfile().expect("tempfile");
     let library_path = library_file.path().to_str().expect("tempfile");
@@ -33,10 +38,14 @@ fn main() {
     let dl_symbols = DlSymbols::new(library_path, "test_suite_aot").expect("dl symbols");
     let aot_symbols = &dl_symbols.aot_symbols;
 
+    let t0 = SystemTime::now();
     let mut machine =
         LlvmAotMachine::new(4 * 1024 * 1024, &aot_symbols).expect("create aot machine");
     machine.load_program(&code, &args).expect("load to run");
     let result = machine.run().expect("run");
+    let t1 = SystemTime::now();
+    let duration = t1.duration_since(t0).expect("time went backwards");
+    println!("Time to run: {:?}", duration);
 
     if result != 0 {
         println!("Error result: {:?}", result);
